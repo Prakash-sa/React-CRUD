6003a1531a53480f6e6028d0655371c8d66ed667
Prakash-sa <sainiprakash525@gmail.com>
2020-03-26 15:25:54 +0530

    Updates

---
+++ precache-manifest.692093337a35addb5751435582c2c6c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28661caa9f2778f603ba122bc557cf8",
+    "url": "/React-CRUD/index.html"
+  },
+  {
+    "revision": "4c85ee0a3734ae1cc523",
+    "url": "/React-CRUD/static/css/main.2468560c.chunk.css"
+  },
+  {
+    "revision": "4eeca368294d03f22865",
+    "url": "/React-CRUD/static/js/2.9c6c92bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-CRUD/static/js/2.9c6c92bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c85ee0a3734ae1cc523",
+    "url": "/React-CRUD/static/js/main.fede83b0.chunk.js"
+  },
+  {
+    "revision": "a0b3092db0353776acf0",
+    "url": "/React-CRUD/static/js/runtime-main.3e372c29.js"
+  }
+]);
